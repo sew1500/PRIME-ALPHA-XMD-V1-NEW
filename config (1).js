975675f84efc513c73e543c8b51fe6218f2e7f2a
𝙿𝚁𝙸𝙼𝙴 𝙰𝙻𝙿𝙷𝙰-𝙼𝙳 𝚅 01.0 <primealpha727@gmail.com>
2025-08-07 20:21:50 +0530

    Update config (1).js

--- config (1).js
+++ config (1).js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/s44p1hN/8psu-Pn-Jo-Zr.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "𝐇𝐄𝐋𝐋𝐎𝐖,𝐈 𝐀𝐌 𝐏𝐑𝐈𝐌𝐄 𝐀𝐋𝐏𝐇𝐀 𝐗𝐌𝐃 𝐯1🤖 𝐈 𝐀𝐌 𝐀𝐋𝐈𝐕𝐄 𝐍𝐎𝐖 👋"'  
 };
